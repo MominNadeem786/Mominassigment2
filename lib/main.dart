a5a9d15edc015084315d49ea4e5873b484b7adcf
mominnadeem786 <mominnadeem1999@gmail.com>
2023-12-22 02:45:43 +0500

    question3

--- lib/main.dart
+++ lib/main.dart
@@ -8,19 +8,19 @@ void main() {
   // print("smallest number:$smallest");
   // print("greatest number:$greatest");
 //   Q:2 emove all false values from below list by using removeWhere or retainWhere property.
-  List<String> usereligibility = [
-    "John",
-    "Alice",
-    "Eligible",
-    "Mike",
-    "Sarah",
-    "Tom"
-  ];
-  usereligibility.removeWhere((element) => element == "Eligible");
-  print(usereligibility);
+  // List<String> usereligibility = [
+  //   "John",
+  //   "Alice",
+  //   "Eligible",
+  //   "Mike",
+  //   "Sarah",
+  //   "Tom"
+  // ];
+  // usereligibility.removeWhere((element) => element == "Eligible");
+  // print(usereligibility);
   //    Q:3  Given a list of integers, write a dart code that returns the maximum value from the list.
-  // List<int> numbers = [5, 2, 4, 8, 1, 3, 6, 7];
-  // int maxvalue =
-  //     numbers.reduce((max, current) => max > current ? max : current);
-  // print("maximun value :$maxvalue");
+  List<int> numbers = [5, 2, 4, 8, 1, 3, 6, 7];
+  int maxvalue =
+      numbers.reduce((max, current) => max > current ? max : current);
+  print("maximun value :$maxvalue");
 }
